13121e0c52fcb5289fc1b1a4c0e71053a91efa71
SIWON990327 <pos06058@naver.com>
2025-06-21 23:44:14 +0900

    프로젝트 생성

---
+++ src/main/java/com/example/solo/TimeChartApplication.java
@@ -0,0 +1,12 @@
+package com.example.solo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeChartApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TimeChartApplication.class, args);
+  }
+}
