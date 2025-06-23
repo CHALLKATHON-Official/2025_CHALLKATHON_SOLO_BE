7dfc148f8407853f44f9126a7b844b385535afcf
SIWON KIM <114036799+NCOOL060580327@users.noreply.github.com>
2025-06-23 10:57:25 +0900

    feat : 월별 일정 조회 구현 (#12)

---
+++ src/main/java/com/example/solo/schedule/application/facade/GetMonthScheduleFacade.java
@@ -0,0 +1,22 @@
+package com.example.solo.schedule.application.facade;
+
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+import com.example.solo.member.domain.entity.Member;
+import com.example.solo.schedule.application.service.ScheduleQueryService;
+import com.example.solo.schedule.domain.dto.response.GetScheduleResponse;
+
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class GetMonthScheduleFacade {
+
+  private final ScheduleQueryService scheduleQueryService;
+
+  public List<GetScheduleResponse> getSchedules(Member member, Integer year, Integer month) {
+    return scheduleQueryService.getSchedules(member, year, month);
+  }
+}
---
+++ src/main/java/com/example/solo/schedule/domain/dto/request/GetMonthScheduleRequestDto.java
@@ -0,0 +1,3 @@
+package com.example.solo.schedule.domain.dto.request;
+
+public record GetMonthScheduleRequestDto(Integer year, Integer month) {}
