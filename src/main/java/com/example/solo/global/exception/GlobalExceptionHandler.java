85a59dd140d4e13d72c782ab122b768b8e77f9b8
SIWON990327 <pos06058@naver.com>
2025-06-26 13:23:48 +0900

    refactor : 일정추가 예외처리

--- src/main/java/com/example/solo/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/solo/global/exception/GlobalExceptionHandler.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import com.example.solo.global.exception.custom.AuthException;
 import com.example.solo.global.exception.custom.MemberException;
+import com.example.solo.global.exception.custom.ScheduleException;
 import com.example.solo.global.exception.custom.TokenException;
 
 import lombok.extern.slf4j.Slf4j;
@@ -38,6 +39,13 @@ public class GlobalExceptionHandler {
   @ExceptionHandler(value = {AuthException.class})
   protected ResponseEntity<BaseResponse<?>> handleCustomException(AuthException e) {
     log.error("handleCustomException throw AuthException : {}", e.getErrorCode());
+    return ResponseEntity.status(e.getErrorCode().getHttpStatus())
+        .body(BaseResponse.onFailure(e.getErrorCode(), null));
+  }
+
+  @ExceptionHandler(value = {ScheduleException.class})
+  protected ResponseEntity<BaseResponse<?>> handleCustomException(ScheduleException e) {
+    log.error("handleCustomException throw ScheduleException : {}", e.getErrorCode());
     return ResponseEntity.status(e.getErrorCode().getHttpStatus())
         .body(BaseResponse.onFailure(e.getErrorCode(), null));
   }
